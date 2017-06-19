18d3196225205b91eade7defde3c1fbba0db6d79
qiqi <qiqi@10.211.55.2>
2017-06-19 14:49:38 +0800

    修改dev_meta

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 # apollo config db info
-apollo_config_db_url=jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8
+apollo_config_db_url=jdbc:mysql://59.111.92.52:3306/ApolloConfigDB?characterEncoding=utf8
 apollo_config_db_username=root
-apollo_config_db_password=
+apollo_config_db_password=123456
 
 # apollo portal db info
-apollo_portal_db_url=jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8
+apollo_portal_db_url=jdbc:mysql://59.111.92.52:3306/ApolloPortalDB?characterEncoding=utf8
 apollo_portal_db_username=root
-apollo_portal_db_password=
+apollo_portal_db_password=123456
 
 # meta server url
-dev_meta=http://localhost:8080
-fat_meta=http://localhost:8080
-uat_meta=http://localhost:8080
-pro_meta=http://localhost:8080
+dev_meta=http://59.111.92.195:8080
+#fat_meta=http://localhost:8080
+#uat_meta=http://localhost:8080
+#pro_meta=http://localhost:8080
 
-META_SERVERS_OPTS="-Ddev_meta=$dev_meta -Dfat_meta=$fat_meta -Duat_meta=$uat_meta -Dpro_meta=$pro_meta"
+META_SERVERS_OPTS="-Ddev_meta=$dev_meta"
 
 # =============== Please do not modify the following content =============== #
 cd ..
